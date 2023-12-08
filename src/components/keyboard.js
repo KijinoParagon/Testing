11a2d884148800e4338b3593cac558365bae5970
KijinoParagon <andrew.honey@southeasttech.edu>
2023-12-08 12:15:19 -0600

    keyboard and ui?

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -2,16 +2,17 @@ AFRAME.registerComponent('keyboardcontrols', {
 
   schema: {
     speed: {type: 'vec3', default: {x: 0, y: 1, z: 0}},
-    hand: {default: 'left'}
+    hand: {default: 'left'},
+    height: {default: 1.6}
   },
 
   init: function () {
     var el = this.el;
     var self = this;
     document.addEventListener('keydown', (evt) => {
-      if(!this.data.speed.y > 0)
+      if(!this.data.speed.y > 0 && evt.key == " ")
       {
-        this.data.speed.y = 1;
+        this.data.speed.y = .5;
       }
     });
 
@@ -21,22 +22,50 @@ AFRAME.registerComponent('keyboardcontrols', {
         this.data.speed.y = 1;
       }
     });
-    
 
-    //document.addEventListener('thumbstickchanged', this.colordebug(0));
-    
-    //document.addEventListener('thumbstickmoved', this.moveupdate);
-    document.addEventListener('thumbstickmoved', function(evt) {
-      self.moveupdate(self, evt);
+    document.addEventListener('keypress', (evt) => {
+      var sp = {x: 0, y: 0, z: 0};
+      switch (evt.key) {
+        case "w":
+          sp.y = -1;
+          break;
+        case "a":
+          sp.x = -1;
+          break;
+        case "s":
+          sp.y = 1;
+          break;
+        case "d":
+          sp.x = 1;
+          break;
+      }
+
+      (sp.x || sp.y) ? this.moveupdate({detail: sp}) : sp.x++;
+    });
+
+    document.addEventListener('keyup', (evt) => {
+      var sp = {x: 0, y: 0, z: 0};
+      switch (evt.key) {
+        case "w":
+          sp.y = 0;
+          break;
+        case "a":
+          sp.x = 0;
+          break;
+        case "s":
+          sp.y = 0;
+          break;
+        case "d":
+          sp.x = 0;
+          break;
+      }
+
+      (!sp.x || !sp.y) ? this.moveupdate({detail: sp}) : sp.x++;
     });
     
-    //document.addEventListener('thumbstickdown', this.colordebug(1));
-    //document.addEventListener('thumbstickup', this.colordebug(2));
-    //document.addEventListener('thumbsticktouchstart', this.colordebug(3));
-    //document.addEventListener('thumbsticktouchend', this.colordebug(4));
-    //document.addEventListener('thumbstickmoved', this.colordebug(5));
-    
-
+    document.addEventListener('thumbstickmoved', function(evt) {
+      self.moveupdate(evt);
+    });
   },
 
   tick: function() {
@@ -44,57 +73,46 @@ AFRAME.registerComponent('keyboardcontrols', {
       pos.x += this.data.speed.x;
       pos.y += this.data.speed.y;
       pos.z += this.data.speed.z;
-      //console.log(speed, pos.y);
-      if(this.data.speed.y > -1 && pos.y > 0) {
+      //console.log(this.data.speed, pos.y);
+      if(this.data.speed.y > -1 && pos.y > this.data.height) {
         this.data.speed.y-=0.05;
       }
-      if(pos.y < 0)
+      if(pos.y < this.data.height)
       {
           this.data.speed.y = 0;
-          pos.y = 0
+          pos.y = this.data.height;
       }
   },
 
-  moveupdate: function(player, evt) {
-
+  moveupdate: function(evt) {
     this.data.speed.x = evt.detail.x/8;
     this.data.speed.z = evt.detail.y/8;
-    
   },
+});
 
-  testfu: function(evt, t) {
-    console.log(t);
-    //console.log(evt.target);
-    console.log(evt);
-    //console.log(x);
-  },
+AFRAME.registerComponent('clickable', {
+  init: function() {
+    console.log(this.el);
+    this.el.addEventListener('onclick', (evt) => {
+      console.log("clicked")
+      var sp = {x: 0, y: 0, z: 0};
+      console.log(evt);
+      /*switch (evt.key) {
+        case "w":
+          sp.y = 0;
+          break;
+        case "a":
+          sp.x = 0;
+          break;
+        case "s":
+          sp.y = 0;
+          break;
+        case "d":
+          sp.x = 0;
+          break;
+      }*/
 
-  colordebug: function(x) {
-    var color = '';
-    switch(x){
-      case 0:
-        color = 'grey';
-        break;
-      case 1:
-        color = 'blue';
-        break;
-      case 2:
-        color = 'black';
-        break;
-      case 3:
-        color = 'purple';
-        break;
-      case 4:
-        color = 'orange';
-        break;
-      case 5:
-        color = 'pink';
-        break;
-      case 6:
-        color = 'yellow';
-        break;
-
-    }
-    document.querySelector('a-sky').setAttribute('color', color)
+    });
   }
+
 });
