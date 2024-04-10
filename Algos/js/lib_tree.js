cfa29f6b1285a039bec9cca49b73e4789ffd4755
KijinoParagon <andrew.honey@southeasttech.edu>
2024-04-10 15:36:39 -0500

    added Algorithms for sharing

---
+++ Algos/js/lib_tree.js
@@ -0,0 +1,76 @@
+var sizeSlide = document.querySelector("#nodeSize");
+var size = parseInt(sizeSlide.value);
+class Leaf {
+  constructor(value)
+  {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function getDepth(tree)
+{
+  var depth = 1;
+    var node = tree;
+    //Find the deepest depth, which will always be to the left
+    while(true)
+    {
+      if(node.left)
+      {
+        node = node.left;
+        depth++;
+      }
+      else {
+        break
+      }
+        
+  }
+
+  return depth;
+}
+
+function insert (newLeaf, tree) {
+    var depth = 1;
+    var node = tree;
+    //Find the deepest depth, which will always be to the left
+    while(true)
+    {
+      if(node.left)
+      {
+        node = node.left;
+        depth++;
+      }
+      else {
+        break
+      }
+        
+    }
+
+    var list = [{node: tree, depth: 1}];
+    while(true){
+      var leaf = list.shift();
+      if (leaf.depth == depth - 1 && !leaf.node.left)
+      {
+        leaf.node.left = newLeaf;
+        break
+      }
+      else if (leaf.depth == depth - 1 && !leaf.node.right)
+      {
+        leaf.node.right = newLeaf;
+        break
+      }
+      else if (leaf.depth < depth - 1)
+      {
+        list.push({node: leaf.node.left, depth: leaf.depth + 1});
+        list.push({node: leaf.node.right, depth: leaf.depth + 1});
+      }
+      else if (!list.length)
+      {
+        node.left = newLeaf;
+        break;
+      }
+
+    }
+    
+}
