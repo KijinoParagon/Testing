aa28fa474ff049895a216830c9a75c52949231df
KijinoParagon <andrew.honey@southeasttech.edu>
2024-11-05 09:20:36 -0600

    Testing Async Load of Header/Template

---
+++ file.js
@@ -0,0 +1,13 @@
+async function getComponent(url) {
+    var response = await fetch(url);
+    var text = await response.text();
+    var component = document.createElement('div');
+    component.innerHTML = text;
+    component = component.firstChild;
+    return component;
+}
+
+async function loadHeader() {
+    var header = await getComponent("https://cdn.jsdelivr.net/gh/KijinoParagon/Portfolio@main/header.html");
+    document.querySelector('header').appendChild(header);
+}
